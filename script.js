8994e595602993840ffb8ec95caa28c4209f3fbf
TejaGopalapurapu <gopalapuraputeja@gmail.com>
2021-11-18 16:01:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var settingsmenu = document.querySelector(".settings-menu");
+
+function SettingsMenuToggle(){
+    settingsmenu.classList.toggle("settings-menu-height")
+}
+var inboxmenu = document.querySelector(".inbox-menu");
+ function InboxMenuToggle(){
+     inboxmenu.classList.toggle("inbox-menu-height")
+ }
